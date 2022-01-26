4ddfd7ea54a269ddc071c00f8f6848163819217d
Shubham Kumar <shubhamkumar@Shubhams-MacBook-Air.local>
2022-01-26 14:01:16 +0530

    added version flag seperately

--- src/mongoose/version-plugin.ts
+++ src/mongoose/version-plugin.ts
@@ -1,21 +1,8 @@
 import { Schema } from "mongoose";
 
 export function versionPlugin(schema: Schema) {
-  const versionKey = schema.get("versionKey");
-
-  if (!versionKey) {
-    throw new Error("VersionKey must be defined");
-  }
-
-  if (typeof versionKey === "boolean") {
-    throw new Error("Version Key cannot be boolean");
-  }
-
-  // pre-save hook to check version
   schema.pre("save", function (next) {
-    // Condition the save on the versions matching
-    this.$where = { ...this.$where, [versionKey]: this[versionKey] };
-    this.increment(); // increamenting version automatically
+    this.version = typeof this.version === "undefined" ? 0 : this.version++;
     next();
   });
 }
