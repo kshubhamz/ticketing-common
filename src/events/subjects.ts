ff593864dbcfe692d837b8a3690fba454278f096
Shubham Kumar <shubhamkumar@Shubhams-MacBook-Air.local>
2022-01-27 14:29:08 +0530

    fixed event typo

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -1,6 +1,6 @@
 export const enum Subjects {
   TICKET_CREATED = "ticket:created",
   TICKET_UPDATED = "ticket:updated",
-  ORDER_CREATED = "order:updated",
+  ORDER_CREATED = "order:created",
   ORDER_CANCELLED = "order:cancelled",
 }
